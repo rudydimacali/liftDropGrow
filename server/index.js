5713df3b949b924bae602d1ba78eb852016eab56
Rudy <rudydimacali@gmail.com>
2019-01-25 17:10:49 -0800

    Create API endpoints.

--- server/index.js
+++ server/index.js
@@ -2,19 +2,61 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
-const { findWorkouts } = require('../database/postgres/index.js');
+const { createWorkoutName, findWorkouts, createWorkoutRecord, findWorkoutNames, findDates } = require('../database/postgres/index.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client/dist'));
 
-findWorkouts(1, (err, res) => {
-  if (err) {
-    console.log('Error!');
-  } else {
-    console.log(res);
-  }
-})
+app.get('/api/workoutNames', (req, res) => {
+  findWorkoutNames((err, names) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.status(200).send(names);
+    }
+  });
+});
+
+app.post('/api/workoutNames', (req, res) => {
+  createWorkoutName(req.body.name, (err, success) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.status(201).send(success);
+    }
+  });
+});
+
+app.get('/api/workouts', (req, res) => {
+  findWorkouts(req.body.date, (err, workouts) => {
+    if (err) {
+      res.status(404).send('Error!');
+    } else {
+      res.status(200).send(workouts);
+    }
+  });
+});
+
+app.post('/api/workouts', (req, res) => {
+  createWorkoutRecord(req.body, (err, success) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.status(201).send(success);
+    }
+  });
+});
+
+app.get('/api/dates', (req, res) => {
+  findDates((err, dates) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.status(200).send(dates);
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
