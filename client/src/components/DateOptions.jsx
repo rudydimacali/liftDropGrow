30b6f411c6f07c08c73633d3c51d304a56661434
Rudy <rudydimacali@gmail.com>
2019-01-26 15:59:32 -0800

    Make date options sort from most recent to oldest.

--- client/src/components/DateOptions.jsx
+++ client/src/components/DateOptions.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+const moment = require('moment');
 
 const DateOptions = ({ dateArray }) => {
+  let sortedDates = dateArray.slice().sort((a, b) => {
+    return moment(b, 'MM/DD/YYYY').format('X') - moment(a, 'MM/DD/YYYY').format('X');
+  });
   return (
-    dateArray.map((date) => {
+    sortedDates.map((date) => {
       return <option value={date} key={date}>{date}</option>
     })
   );
