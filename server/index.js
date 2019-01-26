0e5803ab03acb484f82c0f8799fe5746de4998c7
Rudy <rudydimacali@gmail.com>
2019-01-25 16:01:15 -0800

    Initialize Postgres schema and connection to Express.

--- server/index.js
+++ server/index.js
@@ -2,10 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
+const { queryMainTable } = require('../database/postgres/index.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client/dist'));
+
+queryMainTable(1, (err, res) => {
+  if (err) {
+    console.log('Error!');
+  } else {
+    console.log(res);
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
